088161ecd28610440b32b4e6a8ea4be004de626c
Fernando Jimenez Moreno <ferjmoreno@gmail.com>
2021-06-21 14:57:13 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const chokidar = require('chokidar');
+const connectLivereload = require("connect-livereload");
+const express = require('express');
+const fs = require('fs');
+const livereload = require("livereload");
+const path = require('path');
+const { spawn } = require("child_process");
+
+const app = express();
+
+app.use(connectLivereload());
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, "public")));
+
+let dots = [];
+app.get('', (req, res) => {
+    res.render('index', {
+        dots,
+    });
+});
+
+const dotDir = process.env.GST_DEBUG_DUMP_DOT_DIR;
+const svgDir = './public/svg/';
+
+fs.rmdirSync(svgDir, { recursive: true });
+fs.mkdirSync(svgDir);
+chokidar.watch((dotDir ? dotDir : '.') + '/*dot').on('all', async (event, dotFile) => {
+    const out = svgDir + path.basename(dotFile).replace('.dot', '.svg');
+
+    spawn('dot', ['-Tsvg', dotFile, '-o', out]);
+});
+
+chokidar.watch('public/svg/*svg').on('all', async (event, path) => {
+    dots.push(path.replace('public/', ''));
+    liveReloadServer.refresh("/");
+});
+
+app.listen(3000, () => {
+    console.log('Browse to http://localhost:3000');
+});
+
+const liveReloadServer = livereload.createServer({debug: false});
+liveReloadServer.watch(path.join(__dirname, 'public', 'svg'));
